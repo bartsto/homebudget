34767f93f0a090508b1daacc5dbbe2e6fa86a5a3
Bartosz Stocki <bartek_stocki@wp.pl>
2021-04-22 04:57:14 +0200

    Added sql file with initial data for db

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,7 @@
+INSERT INTO USER (id, username) VALUES (1, 'firstUser');
+INSERT INTO USER (id, username) VALUES (2, 'secondUser');
+
+INSERT INTO REGISTER (id, balance, created_at, name, user_id) VALUES (1, 1000, '2021-04-21 18:00', 'Wallet', 1);
+INSERT INTO REGISTER (id, balance, created_at, name, user_id) VALUES (2, 5000, '2021-04-21 18:00', 'Savings', 1);
+INSERT INTO REGISTER (id, balance, created_at, name, user_id) VALUES (3, 0, '2021-04-21 18:00', 'Insurance Policy', 1);
+INSERT INTO REGISTER (id, balance, created_at, name, user_id) VALUES (4, 0, '2021-04-21 18:00', 'Food expenses', 1);
